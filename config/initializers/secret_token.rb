798b9483d75aa37cb46cf7f6a68f9c6827eb7af9
Abhishek Parolkar <abhishek@parolkar.com>
2011-05-16 18:55:55 +0530

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Childbirth::Application.config.secret_token = '207e4ac8edbab2317c0b787b585520bd0ee04f21fa37eb1db5195dea803abcdb7f1b51bc8df573b92a74373fe1ebddbd67b50a9586d7f03b4f81c5d9f80c6327'
